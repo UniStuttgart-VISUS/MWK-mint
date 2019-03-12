6e927a71ed0de5f165f980b18161e880cf53644c
Sergej Geringer <sergej.geringer@visus.uni-stuttgart.de>
2019-03-12 21:12:07 +0100

    interop: add camera config structs

--- interop/include/interop.hpp
+++ interop/include/interop.hpp
@@ -39,4 +39,75 @@ using uint = unsigned int;
 		void* m_sender;
 	};
 
+	// all vectors, matrices and quaternions follow OpenGL and GLM conventions
+	// in the sense that the data can directly be passed to GL and GLM functions
+
+	// column vector, as in glm
+	struct vec4 {
+		float x = 0.0f;
+		float y = 0.0f;
+		float z = 0.0f;
+		float w = 0.0f;
+	};
+
+	// column major matrix built from column vectors: (v1, v2, v3, v4)
+	struct mat4 {
+		vec4 data[4];
+	};
+
+	struct CameraView {
+		vec4 eyePos;
+		vec4 lookAtPos;
+		vec4 camUp;
+	};
+
+	struct CameraProjection {
+		float fieldOfViewY_deg = 1.0f; // vertical field of view in degrees
+		float nearClipPlane = 0.1f; // distance of near clipping plane
+		float farClipPlane = 1.0f;
+
+		float aspect = 1.0f; // aspect ratio, width divided by height
+		uint pixelWidth = 1; // width of camera area in pixels, i.e. framebuffer texture size
+		uint pixelHeight = 1;
+	};
+
+	struct CameraConfiguration {
+		CameraView viewParameters;
+		CameraProjection projectionParameters;
+
+		// the matrices hold the respective low-level camera transformations as read from unity
+		// the matrices may be from the VR SDK and thus better suited for VR stereo rendering than the explicit camera parameters above
+		mat4 viewMatrix;
+		mat4 projectionMatrix;
+	};
+
+	struct StereoCameraConfiguration {
+		float stereoConvergence = 1.0f; // distance to point where left and right eye converge
+		float stereoSeparation = 1.0f; // distance between virtual eyes
+
+		CameraConfiguration cameraLeftEye;
+		CameraConfiguration cameraRightEye;
+	};
+
+	// to be applied as model matrix (or equivalent) to the dataset before rendering
+	struct ModelConfiguration {
+		vec4 translation;
+		float scale = 1.0f;
+		vec4 rotation_quaternion;
+
+		mat4 modelMatrix;
+	};
+
+	// renderer receives this config from unity
+	struct DatasetRenderConfiguration {
+		StereoCameraConfiguration stereoCamera;
+		ModelConfiguration modelTransform;
+	};
+
+	// renderer sends bounding box (in world space) for its geometry to unity
+	// bounding box is used as reference for rendering and interaction with dataset
+	struct BoundingBoxCorners {
+		vec4 min;
+		vec4 max;
+	};
 }
