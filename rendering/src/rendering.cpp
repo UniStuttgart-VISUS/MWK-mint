66e995601388d2df587fedc5ae4c076057848e35
Sergej Geringer <sergej.geringer@visus.uni-stuttgart.de>
2019-03-15 22:06:54 +0100

    rendering: make window on top of all others, better for debugging

--- rendering/src/rendering.cpp
+++ rendering/src/rendering.cpp
@@ -120,6 +120,7 @@ int main(void)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_FLOATING, GLFW_TRUE);
 	window = glfwCreateWindow(640, 480, "Triangle Rendering", NULL, NULL);
 	if (!window)
 	{
