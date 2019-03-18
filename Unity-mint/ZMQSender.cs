8555f738becf0fd49949c1d212d292c6768c7742
Sergej Geringer <sergej.geringer@visus.uni-stuttgart.de>
2019-03-18 17:44:43 +0100

    Unity-mint: ZMQSender collects all sendables on startup.
    Should be used as attached to central ZMQSender manager object.

--- Unity-mint/ZMQSender.cs
+++ Unity-mint/ZMQSender.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 using NetMQ;
 using NetMQ.Sockets;
+using System.Linq;
+
 
 namespace interop
 {
@@ -16,7 +18,7 @@ public class ZMQSender : MonoBehaviour {
     public string m_address = "tcp://localhost:12345"; // user may set address string where to connect via ZMQ
 
     private PublisherSocket m_socket; // ZMQ Socket
-    private IJsonStringConvertible[] jsons; // scripts attached to this object which may give us json data
+    private (IJsonStringConvertible,string)[] m_convsAndNames; // all Convertible scripts attached to objects in the scene which give us json data
 
 	void Start () {
         AsyncIO.ForceDotNet.Force();
@@ -26,20 +28,37 @@ public class ZMQSender : MonoBehaviour {
             m_socket.Options.SendHighWatermark = 1000;
             m_socket.Bind(m_address);
         }
+        
+        // get all IJsonStringConvertible scripts which are attached to objects in the scene
+        m_convsAndNames = GameObject.FindObjectsOfType<GameObject>() // gelt all GameObjects in scene
+            .Where(o => o.GetComponents<IJsonStringConvertible>().Length > 0) // take only objects which have IJsonStringConvertible component
+            .Select(o => // for each object, make tuple (IJsonStringConvertible[] convertibles, string objectName)
+                (o.GetComponents<IJsonStringConvertible>(), // take IJsonStringConvertible[] from all IJsonStringConvertible in the components
+                Enumerable.Repeat(o.name, o.GetComponents<IJsonStringConvertible>().Length).ToArray()) ) // list with name of object as entries
+            .Select(o => o.Item1.Zip(o.Item2, (conv, name) => (conv, name)).ToArray()) // merge the two seperate Convertible and name arrays to get one array of (IJsonStringConvertible, fatherObjectName)[]
+            .Aggregate(new (IJsonStringConvertible,string)[0], (current, item) =>  current.Concat(item).ToArray()); // merge the multiple arrays into one (IJsonStringConvertible,string)[]
+        // => in the end, we collected into an array all IJsonStringConvertible scripts in the scene with the names of the objects they are attached to
 
-        jsons = gameObject.GetComponents<IJsonStringConvertible>();
+        foreach(var j in m_convsAndNames)
+        {
+            string convName = j.Item1.nameString();
+            string objectName = j.Item2;
+            Debug.Log("ZMQSender found Convertible '"+ convName +"' in Object '" + objectName + "'");
+        }
+        Debug.Log("ZMQSender has " + m_convsAndNames.Length + " convertibles");
     }
 
 	// called once per frame, after all Updates are done
 	void LateUpdate () {
-        if(jsons != null)
+        if(m_convsAndNames != null)
         {
-            foreach(IJsonStringConvertible j in jsons)
+            foreach(var j in m_convsAndNames)
             {
-                string name = j.nameString();
-                string json = j.jsonString();
+                var conv = j.Item1;
+                string name = conv.nameString();
+                string json = conv.jsonString();
 
-                Debug.Log(name + " JsonString: " + json);
+                Debug.Log("'" + j.Item2 + "' sends '" + name + "': " + json);
                 m_socket.SendMoreFrame(name).SendFrame(json);
             }
         }
