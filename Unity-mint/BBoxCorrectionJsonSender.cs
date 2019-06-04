497b112cd9e1bbe3c3afaf66b7c8495f36fd91b0
Sergej Geringer <sergej.geringer@visus.uni-stuttgart.de>
2019-06-04 21:02:15 +0200

    Unity: improve bounding box alignment and add bbox correction sender

---
+++ Unity-mint/BBoxCorrectionJsonSender.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using interop;
+
+[RequireComponent(typeof(BoundingBoxCornersJsonReceiver))]
+public class BBoxCorrectionJsonSender : MonoBehaviour, IJsonStringSendable
+{
+    public string Name = "BBoxCorrection";
+    // Start is called before the first frame update
+
+    private BoundingBoxCornersJsonReceiver m_bboxReceiver = null;
+    void Start()
+    {
+        m_bboxReceiver = gameObject.GetComponent<BoundingBoxCornersJsonReceiver>();
+    }
+
+	public string nameString() {
+        return this.Name;
+	}
+
+	public string jsonString() {
+        ModelPose mp = m_bboxReceiver.bboxCorrectionTransform;
+        string json = mp.json();
+        return json;
+	}
+	
+}
--- Unity-mint/BoundingBoxCornersJsonReceiver.cs
+++ Unity-mint/BoundingBoxCornersJsonReceiver.cs
@@ -7,11 +7,19 @@ using NetMQ.Sockets;
 
 using interop;
 
+public enum DatasetAlignmentMethod {
+    viaRenderer,
+    viaUnity
+}
+
 // Returns transform of the GameObject as Json string in interop format.
 public class BoundingBoxCornersJsonReceiver : MonoBehaviour, IJsonStringReceivable {
 
     public string Name = "BoundingBoxCorners";
     public bool renderBbox = true;
+    public DatasetAlignmentMethod imageAlignment = DatasetAlignmentMethod.viaRenderer;
+    public bool scaleDatasetDown = false;
+    private float m_scaleDownFactor = 1.0f;
 
     private string m_inJsonString = null;
 
@@ -19,6 +27,12 @@ public class BoundingBoxCornersJsonReceiver : MonoBehaviour, IJsonStringReceivab
     private BoundingBoxCorners m_bbox;
     private Mesh m_bboxMesh = null;
     private Material m_bboxMaterial = null;
+
+    private ModelPose m_bboxCorrectionTransform;
+
+    public ModelPose bboxCorrectionTransform {
+        get { return m_bboxCorrectionTransform; }
+    }
 
 	public string nameString() {
         return this.Name;
@@ -48,6 +62,9 @@ public class BoundingBoxCornersJsonReceiver : MonoBehaviour, IJsonStringReceivab
             m_bbox = BoundingBoxCornersFromString();
             setBboxRendering(m_bbox);
             m_isBboxSet = true;
+
+            if(scaleDatasetDown)
+                doScaleDatasetDown();
         }
 
         if(m_bboxMesh && renderBbox)
@@ -55,10 +72,16 @@ public class BoundingBoxCornersJsonReceiver : MonoBehaviour, IJsonStringReceivab
             Graphics.DrawMesh(m_bboxMesh, this.transform.localToWorldMatrix, m_bboxMaterial, 0);
         }
     }
+
+    void doScaleDatasetDown()
+    {
+        this.transform.localScale = this.transform.localScale * m_scaleDownFactor;
+    }
+
     private void setBboxRendering(BoundingBoxCorners bbox)
     {
-        var bboxMin = convert.toUnity(new Vector3(bbox.min.x, bbox.min.y, bbox.min.z));
-        var bboxMax = convert.toUnity(new Vector3(bbox.max.x, bbox.max.y, bbox.max.z));
+        var bboxMin = new Vector3(bbox.min.x, bbox.min.y, bbox.min.z);
+        var bboxMax = new Vector3(bbox.max.x, bbox.max.y, bbox.max.z);
 
         float minX = bboxMin.x;
         float minY = bboxMin.y;
@@ -68,21 +91,57 @@ public class BoundingBoxCornersJsonReceiver : MonoBehaviour, IJsonStringReceivab
         float maxZ = bboxMax.z;
 
         var bboxCenter = bboxMin + (bboxMax - bboxMin)*0.5f;
-        var diff = (bboxMax - bboxMin);
+        var signedDiff = (bboxMax - bboxMin);
+        var diff = new Vector3(Mathf.Abs(signedDiff.x), Mathf.Abs(signedDiff.y), Mathf.Abs(signedDiff.z));
+        m_scaleDownFactor = 1.0f / Mathf.Max(diff.x, diff.y, diff.z);
 
-        var scaleDownTo1 = Vector3.Scale(this.transform.localScale, new Vector3(1.0f / diff.x, 1.0f / diff.y, 1.0f / diff.z));
-        this.transform.localScale = scaleDownTo1;
+        // depending on where the rendered dataset image is aligned, set some offsets
+        // two options: (a) the renderer understands model matrices and will receive a matrix to align the dataset to (0,0,0)
+        // or (b) we align the dataset used in unity to match the world-space bounding box position we received from the renderer (this slightly breaks dataset rotation)
+        Vector3 centering = new Vector3(0.0f, 0.0f, 0.0f);
+        switch(imageAlignment) {
+            case DatasetAlignmentMethod.viaRenderer:
+                centering = -bboxCenter;
+                break;
+            case DatasetAlignmentMethod.viaUnity:
+                centering = new Vector3(0.0f, 0.0f, 0.0f);
+                break;
+        }
+        m_bboxCorrectionTransform.scale = new Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+        m_bboxCorrectionTransform.rotation_axis_angle_rad = new Vector4(0.0f, 0.0f, 0.0f, 0.0f);
+        switch(imageAlignment) {
+            case DatasetAlignmentMethod.viaRenderer:
+                m_bboxCorrectionTransform.translation = convert.toOpenGL(-bboxCenter);
+                m_bboxCorrectionTransform.modelMatrix = Matrix4x4.TRS(m_bboxCorrectionTransform.translation, Quaternion.identity, m_bboxCorrectionTransform.scale);
+                break;
+            case DatasetAlignmentMethod.viaUnity:
+                m_bboxCorrectionTransform.translation = new Vector4(0.0f, 0.0f, 0.0f, 0.0f);;
+                m_bboxCorrectionTransform.modelMatrix = Matrix4x4.identity;
+                break;
+        }
+        Vector3 meshOffset = new Vector3(0.0f, 0.0f, 0.0f);
+        Vector3 colliderOffset = new Vector3(0.0f, 0.0f, 0.0f);
+        switch(imageAlignment) {
+            case DatasetAlignmentMethod.viaRenderer:
+                meshOffset = new Vector3(0.0f, 0.0f, 0.0f);
+                colliderOffset = new Vector3(0.0f, 0.0f, 0.0f);
+                break;
+            case DatasetAlignmentMethod.viaUnity:
+                meshOffset = signedDiff*0.5f + bboxMin;
+                colliderOffset = bboxCenter;
+                break;
+        }
 
-        // center of bbox is set to (0,0,0)
+        // now set the dataset parameters in unity: bounding box wireframe, resized cube mesh, resized mesh collider
         Vector3[] bboxVertices = new Vector3[] {
-            bboxMin                       - bboxCenter, // 0
-            new Vector3(maxX, minY, minZ) - bboxCenter, // 1
-            new Vector3(minX, maxY, minZ) - bboxCenter, // 2
-            new Vector3(maxX, maxY, minZ) - bboxCenter, // 3
-            new Vector3(minX, minY, maxZ) - bboxCenter, // 4
-            new Vector3(maxX, minY, maxZ) - bboxCenter, // 5
-            new Vector3(minX, maxY, maxZ) - bboxCenter, // 6
-            bboxMax                       - bboxCenter, // 7
+            bboxMin                       + centering, // 0
+            new Vector3(maxX, minY, minZ) + centering, // 1
+            new Vector3(minX, maxY, minZ) + centering, // 2
+            new Vector3(maxX, maxY, minZ) + centering, // 3
+            new Vector3(minX, minY, maxZ) + centering, // 4
+            new Vector3(maxX, minY, maxZ) + centering, // 5
+            new Vector3(minX, maxY, maxZ) + centering, // 6
+            bboxMax                       + centering, // 7
         };
 
         // connect wire frame
@@ -115,7 +174,10 @@ public class BoundingBoxCornersJsonReceiver : MonoBehaviour, IJsonStringReceivab
         List<Vector3> newCubeVertices = new List<Vector3>();
         thisCubeMesh.mesh.GetVertices(cubeVertices);
         foreach(var v in cubeVertices)
-            newCubeVertices.Add(Vector3.Scale(v, diff));
+        {
+            var newVertex = Vector3.Scale(v, diff) + meshOffset;
+            newCubeVertices.Add(newVertex);
+        }
         thisCubeMesh.mesh.SetVertices(newCubeVertices);
 
         // set up box collider for controller interaction and so on
@@ -127,5 +189,6 @@ public class BoundingBoxCornersJsonReceiver : MonoBehaviour, IJsonStringReceivab
         }
         thisBoxCollider.enabled = true;
         thisBoxCollider.size = diff;
+        thisBoxCollider.center = colliderOffset;
     }
 }
