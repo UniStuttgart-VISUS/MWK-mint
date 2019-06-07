9d347c730b8d25a2ec7cf93493688bab964039cc
Sergej Geringer <sergej.geringer@visus.uni-stuttgart.de>
2019-06-07 10:53:57 +0200

    Unity: add relative dataset positioning to StereoCameraViewJsonSender

--- Unity-mint/StereoCameraViewJsonSender.cs
+++ Unity-mint/StereoCameraViewJsonSender.cs
@@ -6,23 +6,36 @@ using UnityEngine.XR;
 using interop;
 
 // collects camera pose for left and right eye and makes them available as StereoCameraView in Json format
+// optionally computes the relative pose of camera to some dataset object (if the renderer producing images does not support model matrices)
 public class StereoCameraViewJsonSender : MonoBehaviour, IJsonStringSendable {
 
     public string Name = "StereoCameraView";
 
-    //private Camera m_camera = null;
+    public bool CameraPositionRelative = false;
+    public GameObject PositionRelativeTo = null;
+    public string relativeModeNameSuffix = "Relative";
+
+    private GameObject relativeCameraPositionL = null;
+    private GameObject relativeCameraPositionR = null;
 
     public void Start()
     {
-        //m_camera = GetComponent<Camera>();
-        //if (m_camera == null)
-        //    printError();
-    }
+        if(CameraPositionRelative)
+        {
+            Name = Name + relativeModeNameSuffix;
 
-    private void printError()
-    {
-        string objectName = gameObject.name;
-        Debug.LogError("StereoCameraViewJsonSender in Object '" + objectName + "' is not attached to a Camera Object!");
+            if(PositionRelativeTo == null)
+                PositionRelativeTo = this.gameObject;
+
+            if(relativeCameraPositionL == null)
+            {
+                relativeCameraPositionL = new GameObject("relativeStereoCameraPositionL");
+                relativeCameraPositionR = new GameObject("relativeStereoCameraPositionR");
+
+                relativeCameraPositionL.transform.SetParent(PositionRelativeTo.transform);
+                relativeCameraPositionR.transform.SetParent(PositionRelativeTo.transform);
+            }
+        }
     }
 
 	public string nameString() {
@@ -30,12 +43,6 @@ public class StereoCameraViewJsonSender : MonoBehaviour, IJsonStringSendable {
 	}
 
 	public string jsonString() {
-        //if (m_camera == null)
-        //{
-        //    printError();
-        //    return "";
-        //}
-
         StereoCameraView cc = StereoCameraViewFromXR();
         string json = cc.json();
         return json;
@@ -46,33 +53,33 @@ public class StereoCameraViewJsonSender : MonoBehaviour, IJsonStringSendable {
         StereoCameraView scv;
 
         // TODO opt 1: position relative to VR origin
+        // => solved via parent GameObject?
         Vector3 eyePosL = InputTracking.GetLocalPosition(XRNode.LeftEye) ; //- vrOrigin.transform.position;
         Vector3 eyePosR = InputTracking.GetLocalPosition(XRNode.RightEye); //- vrOrigin.transform.position;
 
         // TODO opt 2: rotate camera around dataset, if dataset can not be rotated in renderer
+        // => additional Rotation to be added to dataset GameObject in Unity beforehand?
         Quaternion eyeRotL = InputTracking.GetLocalRotation(XRNode.LeftEye) ; // * additionalRotation
         Quaternion eyeRotR = InputTracking.GetLocalRotation(XRNode.RightEye); // * additionalRotation
 
-        // TODO opt 3: what happens without inviwo positioning?
-        //if (!EnvConstants.UseInviwoPositioning)
-        //{
-        //    vec1 = vec1 / vec1.magnitude;
-        //    vec2 = vec2 / vec2.magnitude;
-        //}
-
-        //Quaternion addRotation = Quaternion.Inverse(additionalRotationFrom.transform.rotation);
-        //if (additionalRotationFrom != null)
-        //{
-        //    vec1 = RotatePointAroundPivot(vec1, new Vector3(0f, 0f, 0f), addRotation);
-        //    vec2 = RotatePointAroundPivot(vec2, new Vector3(0f, 0f, 0f), addRotation);
-        //    vecFwd = RotatePointAroundPivot(vecFwd, new Vector3(0f, 0f, 0f), addRotation);
-        //    vecUp = RotatePointAroundPivot(vecUp, new Vector3(0f, 0f, 0f), addRotation);
-        //}
+        if(CameraPositionRelative)
+        {
+            computeRelativePose(relativeCameraPositionL.transform, ref eyePosL, ref eyeRotL);
+            computeRelativePose(relativeCameraPositionR.transform, ref eyePosR, ref eyeRotR);
+        }
 
         setEyeView(out scv.leftEyeView, eyePosL, eyeRotL);
         setEyeView(out scv.rightEyeView, eyePosR, eyeRotR);
 
         return scv;
+    }
+
+    private void computeRelativePose(Transform relativeTransform, ref Vector3 eyePos, ref Quaternion eyeRot)
+    {
+            relativeTransform.SetPositionAndRotation(eyePos, eyeRot);
+
+            eyePos = relativeTransform.localPosition;
+            eyeRot = relativeTransform.localRotation;
     }
 
     private void setEyeView(out CameraView view, Vector3 eyePos, Quaternion eyeRot)
